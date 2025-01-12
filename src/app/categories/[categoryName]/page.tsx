f356a6974cb517ca5ffd24cde57ae66421466d05
KJMAN678 <kj.fireman5@gmail.com>
2025-01-12 20:38:00 +0900

    カテゴリーごとのページを表示した際に、カテゴリー一覧へ戻るボタンを設置
    useRouter Hook を使用してカテゴリー一覧に戻る.
    useRouter は next/router だとappフォルダ内ではエラーになるので next/navigation の方を利用.
    Next.js 15 以降のバージョン対応として、React.useを使って params, searchParams をラップ.

--- src/app/categories/[categoryName]/page.tsx
+++ src/app/categories/[categoryName]/page.tsx
@@ -1,16 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import * as React from 'react'
+
 type Props = {
     params: {categoryName: string};
     searchParams: { [key: string]: string | string[] | undefined};
 }
 
 export default function Page({params, searchParams}: Props) {
-    const page = typeof searchParams.page === "string" ? searchParams.page : "1";
+    const _params = React.use(params)
+    const _searchParams = React.use(searchParams)
+    const page = typeof _searchParams.page === "string" ? searchParams.page : "1";
+    const router = useRouter();
     return (
         <div>
             <div>
                 <h1>カテゴリー別一覧画面</h1>
-                <h2>カテゴリー「{params.categoryName}」</h2>
+                <h2>カテゴリー「{_params.categoryName}」</h2>
                 <p>ページ番号: 「{page}」</p>
+                <button onClick={() => {
+                    router.push("/categories");
+                }}>
+                    カテゴリー一覧へ戻る
+                </button>
             </div>
         </div>
     );
