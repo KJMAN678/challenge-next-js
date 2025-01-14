d5a98c90628042f18d7ed1785ca5e7aa7dd6c713
KJMAN678 <kj.fireman5@gmail.com>
2025-01-14 22:11:48 +0900

    現在選択したサイドバーの項目がオレンジ色になるように修正.
    - 現在選択したパスのリンクに area-current:page を追加して現在のページの属性を追加する処理を追加.
    - css で current:page属性に対して文字色をオレンジ色に変更するようにした.

--- src/app/_components/Nav/index.tsx
+++ src/app/_components/Nav/index.tsx
@@ -1,15 +1,26 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "@/app/_components/Nav/style.module.css";
 
+// 現在のページに応じた aria-current="page" 属性を動的に付与する
+function getAriaCurrent(flag: boolean) {
+    return flag ? { "aria-current": "page" as const} : undefined;
+}
+
 export function Nav() {
+    // usePathname() は現在表示している URL の Path を取得する
+    // Client Component でのみ使用できる
+    const pathName = usePathname();
     return (
         <nav className={styles.nav}>
             <ul>
                 <li>
-                    <Link href="/">Home</Link>
+                    <Link href="/" {...getAriaCurrent(pathName === "/")}>Home</Link>
                 </li>
                 <li>
-                    <Link href="/categories">カテゴリー一覧</Link>
+                    <Link href="/categories" {...getAriaCurrent(pathName.startsWith("/categories"))}>カテゴリー一覧</Link>
                 </li>
             </ul>
         </nav>
